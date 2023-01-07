020717ee7ef0d570ecd664c7b62abfbe738467c5
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2023-01-07 16:02:50 +0900

    Update 벡터 매칭.java

--- 백준/Gold/1007. 벡터 매칭/벡터 매칭.java
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     static double answer;
     static int n, t;
-    static int[] ax = new int[21];
-    static int[] ay = new int[21];
-    static boolean[] c = new boolean[21];
+    static int[] ax = new int[21]; //x좌표 배열
+    static int[] ay = new int[21]; //y좌표 배열
+    static boolean[] c = new boolean[21]; //조합 여부
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,7 +18,7 @@ public class Main {
                 ay[j] = sc.nextInt();
             }
             go(0, 0);
-            System.out.printf("%.6f\n", answer);
+            System.out.printf("%.6f\n", answer); 
         }
     }
 
@@ -26,16 +26,16 @@ public class Main {
         if (cnt == n / 2) {
             double x = 0, y = 0;
             for (int i = 0; i < n; i++) {
-                if (c[i]) {
+                if (c[i]) {          //n/2개는 뺄셈
                     x -= ax[i];
                     y -= ay[i];
                 }
-                else {
+                else {              //n/2개는 덧셈
                     x += ax[i];
                     y += ay[i];
                 }
             }
-            answer = Math.min(answer, Math.sqrt(x*x + y*y));
+            answer = Math.min(answer, Math.sqrt(x*x + y*y)); //최솟값 결정
             return;
         }
         if (index == n) return;
