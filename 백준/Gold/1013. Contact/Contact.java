6adaeac9b37f9a6200ded586ddf09f6a399359a6
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2023-01-16 17:25:12 +0900

    [Gold V] Title: Contact, Time: 412 ms, Memory: 21528 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String Vega = "(100+1+|01)+";
+		int T = sc.nextInt();
+
+		for(int i = 0; i < T; i++) {
+			String N = sc.next();
+
+			if(N.matches(Vega)) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+	}
+}
