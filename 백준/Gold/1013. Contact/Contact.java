c46fe51bebeac395e339c5a49782428adef0e7e8
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2023-01-20 22:29:20 +0900

    Update Contact.java

--- 백준/Gold/1013. Contact/Contact.java
+++ 백준/Gold/1013. Contact/Contact.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String Vega = "(100+1+|01)+";
+		String Vega = "(100+1+|01)+"; //정규표현식 사용
 		int T = sc.nextInt();
 
 		for(int i = 0; i < T; i++) {
