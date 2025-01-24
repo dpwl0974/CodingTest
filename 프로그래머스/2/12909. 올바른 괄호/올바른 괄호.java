eb9b0742e89f7b12251a1b68170cdc24978f9751
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2025-01-24 19:00:03 +0900

    [level 2] Title: 올바른 괄호, Time: 5.87 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') {
+                cnt += 1;    
+            }
+            else if(s.charAt(i) == ')') {
+                cnt -= 1;
+                if(cnt < 0) {
+                    break;
+                }
+            }   
+        }
+        
+        if(cnt != 0) {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
