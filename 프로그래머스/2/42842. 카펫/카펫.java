248c392817579550bd192fa66872550f0c60b66c
윤예지 <67728547+dpwl0974@users.noreply.github.com>
2025-03-10 23:12:39 +0900

    [level 2] Title: 카펫, Time: 6.84 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int area = brown + yellow;          //12
+        
+        for(int w = area; w > 1; w--){
+            if(area % w == 0) {            //12, 6, 4, 3, 2
+                int h = area / w;          //1, 2, 3, 4, 6
+                
+                if(w*2 + ((h-2) * 2) == brown) {
+                    answer[0] = w;
+                    answer[1] = h;
+                    return answer;
+                }
+            } 
+        }
+        return answer;
+    }
+}
