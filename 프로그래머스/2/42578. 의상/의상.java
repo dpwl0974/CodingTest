ea1d1a35779477cc1499a3a0b3c1b56d245c20e7
YEAJI YUN <dpwl0974@gmail.com>
2025-08-13 17:48:22 +0900

    update 다른 방법

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,4 +1,25 @@
 import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        
+        for(String[] c: clothes)  {
+            map.put(c[1], map.getOrDefault(c[1], 1) + 1);
+        }       
+        
+        // 경우의 수 
+        for(int i : map.values()) {
+            answer *= i;
+        }
+        
+        // 아무것도 안하는 값 빼기
+        return answer - 1;
+    }
+}
+
+/* import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
@@ -15,3 +36,4 @@ class Solution {
         return answer - 1;
     }
 }
+*/
