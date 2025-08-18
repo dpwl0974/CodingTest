264c9eaafbadb09847f3082642e16436cc7cd623
YEAJI YUN <dpwl0974@gmail.com>
2025-08-18 11:29:58 +0900

    Update 주식가격.java - 2가지 풀이

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,41 @@
-import java.util.*;
+// 또 다른 풀이
 class Solution {
     public int[] solution(int[] prices) {
-        List<Integer> answer = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = prices.length - 1; i >= 0; i--) {stack.push(prices[i]);}
-        
-        while (!stack.isEmpty()) {
-            int check = stack.get(stack.size() - 1);
+        int n = prices.length;
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
             int cnt = 0;
-
-                for (int i = stack.size() - 2; i >= 0; i--) { // 한 칸씩 내려가며 비교
-                    cnt++;
-                if (stack.get(i) < check) break;
-                }
-
-            answer.add(cnt);
-            stack.pop(); 
+            for (int j = i + 1; j < n; j++) {
+                cnt++;
+                if (prices[j] < prices[i]) break; // 떨어지면 멈춤
+            }
+            answer[i] = cnt;
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public int[] solution(int[] prices) {
+//         List<Integer> answer = new ArrayList<>();
+//         Stack<Integer> stack = new Stack<>();
+        
+//         for(int i = prices.length - 1; i >= 0; i--) {stack.push(prices[i]);}
+        
+//         while (!stack.isEmpty()) {
+//             int check = stack.get(stack.size() - 1);
+//             int cnt = 0;
+
+//                 for (int i = stack.size() - 2; i >= 0; i--) { // 한 칸씩 내려가며 비교
+//                     cnt++;
+//                 if (stack.get(i) < check) break;
+//                 }
+
+//             answer.add(cnt);
+//             stack.pop(); 
+//         }
+//         return answer.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
