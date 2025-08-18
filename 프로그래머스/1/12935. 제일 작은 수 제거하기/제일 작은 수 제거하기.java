5a0d7462755ffa131df4d598dc26b3e89b67bbe4
YEAJI YUN <dpwl0974@gmail.com>
2025-08-18 15:37:05 +0900

    Update 제일 작은 수 제거하기.java - 3가지 풀이 방법

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,18 +1,51 @@
 import java.util.*;
+// for문 사용이지만 예외처리 먼저 하여 좋은 코드
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> answer = new ArrayList<>();
+        if (arr.length == 1) return new int[]{-1}; // 예외 처리
+
         int min = arr[0];
-        
-        for(int i = 1; i < arr.length; i++) {
-            min = Math.min(min,arr[i]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
         }
-        
-        for(int i : arr) {
-            if(i != min) answer.add(i);
+
+        int[] result = new int[arr.length - 1];
+        int idx = 0;
+        for (int num : arr) {
+            if (num != min) result[idx++] = num;
         }
-        if(answer.isEmpty()) answer.add(-1);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+
+        return result;
     }
 }
+
+
+/* 스트림 방식 
+class Solution {
+    public int[] solution(int[] arr) {
+        int min = Arrays.stream(arr).min().getAsInt();
+        int[] result = Arrays.stream(arr)
+                             .filter(x -> x != min)
+                             .toArray();
+        return result.length == 0 ? new int[]{-1} : result;
+    }
+} */
+
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+//         List<Integer> answer = new ArrayList<>();
+//         int min = arr[0];
+        
+//         for(int i = 1; i < arr.length; i++) {
+//             min = Math.min(min,arr[i]);
+//         }
+        
+//         for(int i : arr) {
+//             if(i != min) answer.add(i);
+//         }
+//         if(answer.isEmpty()) answer.add(-1);
+        
+//         return answer.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
