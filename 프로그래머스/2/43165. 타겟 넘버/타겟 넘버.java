11d369719d0c328419001f5083e0aa72579a00ee
YEAJI YUN <dpwl0974@gmail.com>
2025-08-19 09:21:02 +0900

    [level 2] Title: 타겟 넘버, Time: 6.44 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0); // 0부터 시작
+    }
+    
+    public int dfs(int[] numbers, int target, int idx, int sum) {
+        // 순회 완료 -> sum과 target 비교
+        if(idx == numbers.length) return sum == target ? 1 : 0; 
+        
+        // idx 더하거나 빼서 결과 확인
+        return dfs(numbers, target, idx+1, sum + numbers[idx])
+            + dfs(numbers, target, idx+1, sum - numbers[idx]);
+    }
+}
