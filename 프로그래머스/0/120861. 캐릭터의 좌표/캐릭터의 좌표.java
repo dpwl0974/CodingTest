bfbcf8628a5513c4c96b51ad2ed2f620169afa4d
YEAJI YUN <dpwl0974@gmail.com>
2025-08-22 17:45:12 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.10 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -4,29 +4,29 @@ class Solution {
         int x = 0, y = 0;
         
         int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
+        int[] dy = {-1, 1, 0, 0};
         
-        int xLimit = board[0] / 2; // 가로 최대 이동 가능 범위
-        int yLimit = board[1] / 2; // 세로 최대 이동 가능 범위
+        // 범위 잡기
+        int xLimit = board[0] / 2;
+        int yLimit = board[1] / 2;
         
-        HashMap<String, Integer> dirMap = new HashMap<>();
-        dirMap.put("up", 0);  // 위
-        dirMap.put("down", 1);  // 아래
-        dirMap.put("left", 2);  // 왼쪽
-        dirMap.put("right", 3);  // 오른쪽
-
-        for (String str : keyinput) {
-            int dir = dirMap.get(str);  // 명령어 → 인덱스
-            int nx = x + dx[dir];  // x 좌표 이동
-            int ny = y + dy[dir];  // y 좌표 이동    
+        Map<String, Integer> dirmap = new HashMap<>();
+        dirmap.put("down", 0);
+        dirmap.put("up", 1);
+        dirmap.put("left", 2);
+        dirmap.put("right", 3);
+        
+        for(String str : keyinput) {
+            int dir = dirmap.get(str);
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
             
-            if (Math.abs(nx) <= xLimit && Math.abs(ny) <= yLimit) {
-                x = nx; // 유효하면 이동 확정
+            // 범위 체크 -> 절댓값
+            if(Math.abs(nx) <= xLimit && Math.abs(ny) <= yLimit){
+                x = nx;
                 y = ny;
             }
         }
-        
-        
         return new int[] {x, y};
     }
 }
