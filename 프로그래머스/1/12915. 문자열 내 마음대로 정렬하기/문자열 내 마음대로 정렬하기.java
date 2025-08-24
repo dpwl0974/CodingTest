62f62a0a546fef2621c110bf8cb18f5838245a0c
YEAJI YUN <dpwl0974@gmail.com>
2025-08-24 14:54:48 +0900

    chore: 문자열 내 마음대로 정렬하기.java 주석 수정

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -6,7 +6,7 @@ class Solution {
             public int compare(String s1, String s2) {
                 // 먼저 n번째 문자 비교
                 if (s1.charAt(n) != s2.charAt(n)) {
-                    return Character.compare(s1.charAt(n), s2.charAt(n)); // 아스키코드 값 비교 (양수면 s1, 음수면 s2가 크다)
+                    return Character.compare(s1.charAt(n), s2.charAt(n)); // 아스키코드 값 비교 (양수면 s2, 음수면 s1이 크다)
                 }
                 // n번째 문자가 같으면 사전순 비교
                 return s1.compareTo(s2); // 문자열 전체 사전순
