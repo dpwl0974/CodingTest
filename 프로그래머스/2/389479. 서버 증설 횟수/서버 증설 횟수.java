97a6517c656db8c3308d48e23c6a8c0c0b407096
YEAJI YUN <dpwl0974@gmail.com>
2025-08-25 18:03:30 +0900

    Update 서버 증설 횟수.java

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,19 +1,19 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        int[] work = new int[players.length + k]; // 퇴근 시간 저장
-        int worker = 0; // 일하는 직원 수
+        int[] work = new int[players.length + k]; // 반납 시간 저장
+        int worker = 0; // 운영 중 서버
         
         for(int i = 0; i < players.length; i++) {
-            worker -= work[i]; // 퇴근하면 직원 수 빼기
+            worker -= work[i]; // 반납 된 서버 빼기
             
             int required = players[i] / m;
             
             if(required > worker) {
-                int hire = required - worker;
-                answer += hire;
-                worker += hire;
-                work[i + k] += hire;
+                int server = required - worker;
+                answer += server;
+                worker += server;
+                work[i + k] += server;
             }            
         }
         return answer;
