2d768c0df3550db02b0c6813871d5e18b6e87845
YEAJI YUN <dpwl0974@gmail.com>
2025-08-27 18:13:34 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        while(hp > 0){
+            if(hp >= 5) {
+                answer += hp / 5;
+                hp = hp % 5;
+            }
+            else if(hp < 5 && hp >= 3) {
+                answer += hp / 3;
+                hp = hp % 3;
+            }
+            else {
+                hp--;
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
