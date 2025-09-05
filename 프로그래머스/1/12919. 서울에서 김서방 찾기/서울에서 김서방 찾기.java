1f17735ad2161ed8afee6e8546261344f5cb21c7
YEAJI YUN <dpwl0974@gmail.com>
2025-09-05 09:08:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.96 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -4,9 +4,9 @@ class Solution {
         int i = 0;
         
         for(String s : seoul){
-            if(s.equals("Kim")) answer = "김서방은 " + i + "에 있다";
+            if(s.equals("Kim")) break;  // break 통해 배열 그만 돌며 효율성 업
             i++;
         }
-        return answer;
+        return "김서방은 " + i + "에 있다";
     }
 }
