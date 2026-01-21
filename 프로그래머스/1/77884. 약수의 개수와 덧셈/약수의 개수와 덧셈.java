af685efcb735516f97a26100ed2b65066e1b6871
YEAJI YUN <dpwl0974@gmail.com>
2026-01-21 14:21:39 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.08 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -3,23 +3,22 @@ class Solution {
         int answer = 0;
         
         for(int i = left; i <= right; i++){
-            int count = divisor(i);
-            
-            if(count % 2 == 0) answer += i;
-            else answer -= i;
+            if(i % Math.sqrt(i) == 0) answer -= i;
+            else answer += i;
         }
         return answer;
     }
-    
-    public int divisor(int n){
-        int sqrt = (int) Math.sqrt(n);
-        int count = 0;
-        for(int i = 1; i <= sqrt; i++) {
-            if(n % i == 0) {
-                count++;            
-                if(n / i != i) count++;
-            }
-        }
-        return count;
-    }
 }
+    
+//     public int divisor(int n){
+//         int sqrt = (int) Math.sqrt(n);
+//         int count = 0;
+//         for(int i = 1; i <= sqrt; i++) {
+//             if(n % i == 0) {
+//                 count++;            
+//                 if(n / i != i) count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
